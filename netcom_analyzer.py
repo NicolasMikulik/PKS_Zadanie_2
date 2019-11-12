236e13a8d559ba5c5703c5801de2a59a3febf670
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-11-12 07:03:56 +0100

    pt2 print acknowledgement number of fourth frame

--- netcom_analyzer.py
+++ netcom_analyzer.py
@@ -1,11 +1,9 @@
 import struct
 
 fh = open("/home/nicolas/Documents/FIIT/PKS/Zadanie_2/vzorky_pcap_na_analyzu/eth-8.pcap", "rb")
-byte = fh.read(75)
-# while byte:
-byte = fh.read(1)
-first = ord(byte) & 240
-second = ord(byte) & 15
-print(ord(byte), first, second, hex(ord(byte))[2:], hex(first), hex(second))
-# print(ord(fh.read(1)))
+byte = fh.read(406)
+byte = fh.read(4)
+print(byte)
+port = struct.unpack('>I', byte)
+print(port[0])
 fh.close()
